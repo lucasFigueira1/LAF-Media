f702c6854c1947fab7e6a1ff2ec1b654bd2940be
lucasFigueira1 <lucasfigueira137@gmail.com>
2022-12-31 17:22:01 -0300

    Website ready to deploy

--- assets/main.js
+++ assets/main.js
@@ -19,28 +19,37 @@ const changeLanguage = async (language) => {
 /*const selectedLang = localStorage.getItem('selected-lang')
 const selectedFlag = localStorage.getItem('selected-flag')*/
 
+function changeFlagAnimation(language) {
+  const esFlag = document.getElementById("esFlag");
+  const ukFlag = document.getElementById("ukFlag");
+
+  if (language == "es") {
+    ukFlag.classList.remove('flag-active');
+    esFlag.classList.toggle("flag-active");
+
+  } else if (language == "en") {
+    esFlag.classList.remove("flag-active");
+    ukFlag.classList.toggle('flag-active');
+  }
+}
+
 // We obtain the current lang by validating the value of 
-/*const getCurrentLang = () => languageFlags.contains("es") ? 'es' : 'en'*/
+window.addEventListener('load', function() {
+  changeLanguage(getCurrentLang())
+  changeFlagAnimation(getCurrentLang())
+});
+
+const getCurrentLang = () => window.localStorage.getItem('selected-lang') == 'es' ? 'es' : 'en' 
 
 // Change language when click one or another flag
 languageFlags.addEventListener("click", (e) => {
-  const esFlag = document.getElementById("esFlag");
-  const ukFlag = document.getElementById("ukFlag");
   const actualLang = e.target.dataset.language;
 
   changeLanguage(actualLang);
 
   // Add or remove Active flag animation
-  if (actualLang == "es") {
-    ukFlag.classList.remove('flag-active');
-    esFlag.classList.toggle("flag-active");
+  changeFlagAnimation(actualLang);
 
-  } else if (actualLang == "en") {
-    esFlag.classList.remove("flag-active");
-    ukFlag.classList.toggle('flag-active');
-  }
-
-  // Saving current flag and lang that user chose
-  /*localStorage.setItem('selected-lang', getCurrentLang())
-  localStorage.setItem('selected-flag', getCurrentFlag()) */
+  // Saving current lang that user chose
+  window.localStorage.setItem('selected-lang', actualLang)
 });
